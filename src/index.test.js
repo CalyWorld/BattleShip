d3fcc2093078f2d177c6d6938c260c6962416c16
CalAnizoba <obinnaanizoba5@gmail.com>
2022-06-11 21:14:38 +0800

    test if game board is empty

--- src/index.test.js
+++ src/index.test.js
@@ -1,18 +1,18 @@
-import {shipFactory} from "./index"
-import {gameboardFactory} from './index';
+import { shipFactory } from "./index"
+import { gameboardFactory } from './index';
 
-test("Should return ship length",()=>{
+test("Should return ship length", () => {
     let ship = shipFactory(2);
     expect(ship.getShipLength()).toEqual(2);
 });
 
-test("returns the position where the ship was hit", ()=>{
+test("returns the position where the ship was hit", () => {
     let ship = shipFactory(5);
     ship.hit(2);
     expect(ship.getShipArray().at(2)).toBe("hit");
 });
 
-test("should use shipLength and check if all positions have been hit", ()=>{
+test("should use shipLength and check if all positions have been hit", () => {
     let ship = shipFactory(4);
     ship.hit(0);
     ship.hit(1);
@@ -21,10 +21,14 @@ test("should use shipLength and check if all positions have been hit", ()=>{
     expect(ship.isSunk(false)).toEqual(false);  // returns false cause shipArray still has index 3 empty
 });
 
-test("return the position where ship was placed in Gameboard", ()=>{
-    let gameboard = gameboardFactory(5);
-    let ship = shipFactory();
-    gameboard.placeShipOnGameboard(2);
-    expect(gameboard.getGameboardArray().at(2)).toEqual(["","", ship.getShipArray(), "", ""]);
-})
+test("check if gameboard is empty", () => {
+    let gameboard = gameboardFactory();
+    expect(gameboard.showGameboard()[0][0]).toEqual("");
+});
+
+test("check if ship is placed at gameboard coordinates", () => {
+    let gameboard = gameboardFactory();
+    gameboard.placeShip(0, 0, horizontal, "battleship");
+    expect(gameboard.showGameboard()[0][0]).toEqual("battleship");
+});
 
