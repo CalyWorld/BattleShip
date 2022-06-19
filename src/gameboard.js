5d4439aa9bf66028628e42acb3f9192e91cc8a91
CalAnizoba <obinnaanizoba5@gmail.com>
2022-06-20 03:06:36 +0800

    fixed the placeship functions to checked for empty gameboard cell and ship overlap

--- src/gameboard.js
+++ src/gameboard.js
@@ -1,38 +1,38 @@
 import { shipFactory } from "./shipFactory";
 function gameboardFactory() {
-    
+
     let gameboardArray = Array(10).fill("").map(x => Array(10).fill(""));
 
     const showGameboard = () => [...gameboardArray];
 
     const placeShip = (row, column, ship, direction) => {
-        let shipFit = shipfit(row, column, direction, ship.length);
-        let checkSpace = cellEmpty(row, column, direction, ship.length);
-        // if (!shipFit) return false;
-        // if (!checkSpace) return false;
+        let shipFit = shipfit(row, column, direction, ship.getShipLength());
+        let checkSpace = cellEmpty(row, column, direction, ship.getShipLength());
+        if (!shipFit) return false;
+        if (!checkSpace) return false;
         if (direction == "horizontal") {
-            for (let i = 0; i < ship.length; i++) {
-                gameboardArray[column][row + i] = ship;
-                console.table(gameboardArray);
+            for (let i = 0; i < ship.getShipLength(); i++) {
+                gameboardArray[row][column + i] = ship;
             }
             return true;
         }
         else if (direction == "vertical") {
-            for (let i = 0; i < ship.length; i++) {
-                gameboardArray[column + i][row] = ship;
+            for (let i = 0; i < ship.getShipLength(); i++) {
+                gameboardArray[row + i][column] = ship;
             }
             return true;
         }
     }
+
     const shipfit = (row, column, direction, length) => {
         if (direction == "horizontal") {
-            if (row >= 0 && (row + length - 1) < gameboardArray.length) {
+            if (column >= 0 && (column + length - 1) < gameboardArray.length) {
                 return true;
             } else {
                 return false;
             }
         } else if (direction == "vertical") {
-            if (column >= 0 && (column + length - 1) < gameboardArray.length) {
+            if (row >= 0 && (row + length - 1) < gameboardArray.length) {
                 return true;
             } else {
                 return false;
@@ -41,34 +41,32 @@ function gameboardFactory() {
     }
 
     const cellEmpty = (row, column, direction, length) => {
-        if (direction = "horizontal") {
+        if (direction == "horizontal") {
             for (let i = 0; i < length; i++) {
-                if (gameboardArray[column][row + i] !== "") {
+                if (gameboardArray[row][column + i] !== "") {
                     return false;
-                } else {
-                    return true;
                 }
             }
+            return true;
         }
-        else if (direction = "vertical") {
+        else if (direction == "vertical") {
             for (let i = 0; i < length; i++) {
-                if (gameboardArray[column + i][row] !== "") {
+                if (gameboardArray[row + i][column] !== "") {
                     return false;
-                } else {
-                    return true;
                 }
             }
+            return true;
         }
     }
 
-    const recieveAttack = (x, y, direction) => {
-        if (direction == "horizontal") {
-            if (gameboardArray[y][x] !== "") {
-                gameboardArray[y][x].hit();
-                console.table(gameboardArray);
-            }
-        }
-    }
+    // const recieveAttack = (x, y, direction) => {
+    //     if (direction == "horizontal") {
+    //         if (gameboardArray[y][x] !== "") {
+    //             gameboardArray[y][x].hit();
+    //             console.table(gameboardArray);
+    //         }
+    //     }
+    // }
 
     // const checkMiss = (x,y,direction)=>{
     //     let checkBoard = true;
@@ -81,7 +79,7 @@ function gameboardFactory() {
         placeShip,
         shipfit,
         cellEmpty,
-        recieveAttack
+        // recieveAttack
     }
 }
 
--- src/gameboard.test.js
+++ src/gameboard.test.js
@@ -11,56 +11,57 @@ test("check if ship is placed at gameboard coordinate horizontally", () => {
     let ship = shipFactory(4);
     expect(gameboard.placeShip(0, 0, ship, "horizontal")).toEqual(true);
     expect(gameboard.placeShip(0, 5, ship, "horizontal")).toEqual(true);
-    expect(gameboard.placeShip(0, 6, ship, "horizontal")).toEqual(true);
-    expect(gameboard.placeShip(0, 7, ship, "horizontal")).toEqual(true);
+    expect(gameboard.placeShip(0, 6, ship, "horizontal")).toEqual(false);
+    expect(gameboard.placeShip(0, 7, ship, "horizontal")).toEqual(false);
 });
 
 test("check if ship is placed at gameboard coordinate vertically", () => {
     let gameboard = gameboardFactory();
     let ship = shipFactory(2);
     expect(gameboard.placeShip(5, 0, ship, "vertical")).toEqual(true);
-    expect(gameboard.placeShip(6, 0, ship, "vertical")).toEqual(true);
+    expect(gameboard.placeShip(6, 0, ship, "vertical")).toEqual(false);
     expect(gameboard.placeShip(7, 0, ship, "vertical")).toEqual(true);
-    expect(gameboard.placeShip(8, 0, ship, "vertical")).toEqual(true);
+    expect(gameboard.placeShip(8, 0, ship, "vertical")).toEqual(false);
 });
 
 test("check if ship length overlaps gameboard horizontally", () => {
     let gameboard = gameboardFactory();
     expect(gameboard.shipfit(0, 0, "horizontal", 4)).toEqual(true);
-    expect(gameboard.shipfit(6, 0, "horizontal", 4)).toEqual(true);
-    expect(gameboard.shipfit(-8, 0, "horizontal", 4)).toEqual(false);
-    expect(gameboard.shipfit(10, 0, "horizontal", 4)).toEqual(false);
+    expect(gameboard.shipfit(0, 6, "horizontal", 4)).toEqual(true);
+    expect(gameboard.shipfit(0, 8, "horizontal", 4)).toEqual(false);
+    expect(gameboard.shipfit(0, 10, "horizontal", 4)).toEqual(false);
 });
 
 test("check if ship length overlaps gameboard vertically", () => {
     let gameboard = gameboardFactory();
-    expect(gameboard.shipfit(0, 6, "vertical", 4)).toEqual(true);
-    expect(gameboard.shipfit(0, 6, "vertical", 5)).toEqual(false);
-    expect(gameboard.shipfit(0, 0, "vertical", 4)).toEqual(true);
+    expect(gameboard.shipfit(6, 0, "vertical", 4)).toEqual(true);
+    expect(gameboard.shipfit(0, 0, "vertical", 5)).toEqual(true);
+    expect(gameboard.shipfit(5, 0, "vertical", 4)).toEqual(true);
+    expect(gameboard.shipfit(10, 0, "vertical", 4)).toEqual(false);
 });
 
 test("ships can't stack over each other horizontally", () => {
     let gameboard = gameboardFactory();
-    let ship = shipFactory(4);
-    gameboard.showGameboard()[0][5] = ship;
-    expect(gameboard.cellEmpty(5, 0, "horizontal", 4)).toEqual(false);
-    expect(gameboard.cellEmpty(4, 0, "horizontal", 4)).toEqual(true);
-    expect(gameboard.cellEmpty(6, 0, "horizontal", 4)).toEqual(true);
+    let ship = shipFactory(4)
+    gameboard.placeShip(0, 5, ship,"horizontal");
+    expect(gameboard.cellEmpty(0, 5, "horizontal", 4)).toEqual(false);
+    expect(gameboard.cellEmpty(0, 4, "horizontal", 4)).toEqual(false);
+    expect(gameboard.cellEmpty(0, 6, "horizontal", 4)).toEqual(false);
     expect(gameboard.cellEmpty(0, 0, "horizontal", 4)).toEqual(true);
 });
 
 test("ships can't stack over each other vertically", () => {
     let gameboard = gameboardFactory();
     let ship = shipFactory(4);
-    gameboard.showGameboard()[5][0] = ship;
-    expect(gameboard.cellEmpty(0, 5, "vertical", 4)).toEqual(false);
-    expect(gameboard.cellEmpty(0, 4, "vertical", 4)).toEqual(true);
+    gameboard.placeShip(5, 0, ship,"vertical");
+    expect(gameboard.cellEmpty(5, 0, "vertical", 4)).toEqual(false);
+    expect(gameboard.cellEmpty(4, 0, "vertical", 4)).toEqual(false);
     expect(gameboard.cellEmpty(0, 0, "vertical", 4)).toEqual(true);
-    expect(gameboard.cellEmpty(0, 8, "vertical", 4)).toEqual(true);
+    expect(gameboard.cellEmpty(8, 0, "vertical", 4)).toEqual(false);
 });
 
 
-test("gameboard should recieve attack on horizontal coordinate when there is ship", ()=>{
+test("gameboard should recieve attack on horizontal coordinate", ()=>{
     let gameboard = gameboardFactory();
     let ship = shipFactory(4);
     gameboard.showGameboard()[0][5] = ship;
