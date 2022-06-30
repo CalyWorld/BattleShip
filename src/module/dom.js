9f76416aa08d44b440ee70ca73356987dd8f1ffd
CalAnizoba <obinnaanizoba5@gmail.com>
2022-07-01 00:42:53 +0800

    display gameboard

--- src/module/dom.js
+++ src/module/dom.js
@@ -1,7 +1,26 @@
+import { Game } from "./game";
 
-const playerBoard = document.querySelector("playerBoard");
-const computerBoard = document.querySelector("computerBoard");
+const renderPlayerBoard = () => {
+    const playerBoard = document.querySelector(".playerBoard");
+    let gameboard = Game().getPlayer().getGameboard().showGameboard();
+    playerBoard.style.setProperty('--grid-rows', gameboard.length);
+    playerBoard.style.setProperty('--grid-cols', gameboard.length);
+    for (let c = 0; c < (gameboard.length * gameboard.length); c++) {
+        let cell = document.createElement("div");
+        playerBoard.appendChild(cell).className = "grid-item";
+    }
+};
 
-const renderPlayerBoard = () =>{
-    
-}
+const renderComputerBoard = () => {
+    const computerBoard = document.querySelector(".computerBoard");
+    let gameboard = Game().getPlayer().getGameboard().showGameboard();
+    computerBoard.style.setProperty('--grid-rows', gameboard.length);
+    computerBoard.style.setProperty('--grid-cols', gameboard.length);
+    for (let c = 0; c < (gameboard.length* gameboard.length); c++) {
+        let cell = document.createElement("div");
+        computerBoard.appendChild(cell).className = "grid-item";
+    }
+};
+
+export { renderPlayerBoard, renderComputerBoard }
+
