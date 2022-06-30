7df901702375ba57790869a0762a18d32ab7ea0e
CalAnizoba <obinnaanizoba5@gmail.com>
2022-06-30 21:01:56 +0800

    Working on implementing gameboard module

--- src/module/game.js
+++ src/module/game.js
@@ -1,44 +1,52 @@
-import { gameboardFactory } from "./gameboard";
 import { computerFactory, playerFactory } from "./player";
 
-const Game = (()=>{
+export const Game = (() => {
 
-    let ship = shipFactory(5);
     let player = playerFactory("leo", 1);
     let computer = computerFactory("computer", 2);
+    const getPlayer = () => player;
+    const getComputerPlayer = () => computer
 
-    let activePlayer = player;
-    let opponentPlayer = computer.getGameboard();
+    let activePlayer = getPlayer();
+    let opponentPlayer = getComputerPlayer();
 
-    const shipPlacements = () =>{
-        activePlayer.getGameboard().placeShip(0,0,ship,"horizontal");
-        activePlayer.getGameboard().placeShip(0,0,ship,"horizontal");
-        activePlayer.getGameboard().placeShip(0,0,ship,"horizontal");
+    const shipPlacements = () => {
+        activePlayer.getGameboard().placeShip(0, 0, ship, "horizontal");
+        activePlayer.getGameboard().placeShip(0, 1, ship, "horizontal");
+        activePlayer.getGameboard().placeShip(0, 2, ship, "horizontal");
 
-        opponentPlayer.getGameboard().placeShip(x,y,ship,"horizontal"),
-        opponentPlayer.getGameboard().placeShip(x,y,ship,"horizontal");
-        opponentPlayer.getGameboard().placeShip(x,y,ship,"horizontal");
+        opponentPlayer.getGameboard().placeShip(x, y, ship, "horizontal"),
+        opponentPlayer.getGameboard().placeShip(x, y, ship, "horizontal");
+        opponentPlayer.getGameboard().placeShip(x, y, ship, "horizontal");
     }
 
-    const attackShip = () =>{
-        activePlayer.sendAttack(x,y,ship);
+    const attackShip = () => {
+        activePlayer.sendAttack(x, y, opponentPlayer);
+        gameOver();
+        activePlayer.sendAttack(x, y, opponentPlayer);
+        gameOver();
+        activePlayer.sendAttack(x, y, opponentPlayer);
+        gameOver();
     }
 
-    const gameOver = () =>{
-        if(activePlayer.getGameboard().shipSunk == true){
+
+    const gameOver = () => {
+        if (activePlayer.getGameboard().shipSunk == true) {
             console.log(`${computer.getComputerAiName()} is the winner`);
-        }else if(opponentPlayer.getGameboard().shipSunk == true){
+        } else if (opponentPlayer.getGameboard().shipSunk == true) {
             console.log(`${player.getPlayerName()} is the winner`);
-        }else if(activePlayer.getGameboard().shipSunk == false && opponentPlayer.getGameboard().shipSunk == false){
+        } else if (activePlayer.getGameboard().shipSunk == false && opponentPlayer.getGameboard().shipSunk == false) {
             return false;
         }
     };
 
-    return{
+    return {
         shipPlacements,
-        gameOver
+        gameOver,
+        attackShip,
+        getPlayer,
+        getComputerPlayer,
     }
 
 });
 
-export {Game};
