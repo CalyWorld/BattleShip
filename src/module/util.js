7a5548e4180d3224e90900d5f4913d86240f632b
CalAnizoba <obinnaanizoba5@gmail.com>
2022-07-05 01:07:26 +0800

    created ships

---
+++ src/module/util.js
@@ -0,0 +1,21 @@
+import {shipFactory} from "./shipFactory";
+
+const utils = () =>{
+    const createdShips = []
+    let BattleShip = shipFactory(4, "BattleShip");
+    let Carrier = shipFactory(5, "Carrier");
+    let Destroyer = shipFactory(4, "Destroyer");
+    let Submarine = shipFactory(4, "Submarine");
+    let PatrolBoat = shipFactory(2, "Patrol-Boat");
+
+    createdShips.push(BattleShip,Carrier,Destroyer,Submarine,PatrolBoat);
+
+    const getCreatedShips = () => [...createdShips]
+
+    return{
+        getCreatedShips
+    }
+    
+}
+
+export{utils}
