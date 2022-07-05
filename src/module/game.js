e9ce40071558b7f14e290773ff0983b1f782801f
CalAnizoba <obinnaanizoba5@gmail.com>
2022-07-05 15:48:37 +0800

    added playerAttack and computerAttack functions

--- src/module/game.js
+++ src/module/game.js
@@ -4,20 +4,39 @@ const Game = (() => {
 
     let player = playerFactory("leo", 1);
     let computer = computerFactory("computer", 2);
+    let ships = utils().getCreatedShips();
 
 
     const getPlayer = () => player;
     const getComputerPlayer = () => computer;
 
     const placePlayerShip = (row,column,ship,direction)=>{
-        player.getGameboard().placeShip(row,column,ship,direction);
+        return player.getGameboard().placeShip(row,column,ship,direction);
     };
 
-    for(let i = 0; i<utils().getCreatedShips().length; i++){
-        computer.placeShipRandomly(utils().getCreatedShips()[i]);
+    for(let i = 0; i<ships.length; i++){
+        computer.placeShipRandomly(ships[i]);
     };
 
-    // console.table(computer.getGameboard().showGameboard());
+    const playerAttack = (row,column,computer) =>{
+       return player.sendAttack(row,column,computer);
+    }
+
+    const computerAttack = (playerToAttack) =>{
+        return computer.sendAttack(playerToAttack);
+    }
+
+    const playerTurn = (row,column) => {
+        let activePlayer = playerAttack(row,column,computer);
+        if(!activePlayer) return;
+        if(computer.getGameboard().shipSunk()){
+            return player;
+        }
+        computerAttack(activePlayer);
+        if(activePlayer.getGameboard().shipSunk()){
+            return computer;
+        }
+    }
 
     const gameOver = () => {
         if (player.getGameboard().isSunk == true) {
@@ -34,6 +53,9 @@ const Game = (() => {
         getPlayer,
         getComputerPlayer,
         placePlayerShip,
+        computerAttack,
+        playerAttack,
+        playerTurn
     }
 
 });
